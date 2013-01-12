329bd064db1dfd2104cc772ff43f767d75fb2814
Jonathan Treble <jtreble@gmail.com>
2013-01-12 16:08:13 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TabletStore::Application.config.secret_token = '747bafcd33d4554d3e7c0e9cd4e3259369fa8bba423a5c07a87417457974d5a6fc6696b8ee46a714d8ea2a800279d2c51c960dd7ac7bcb708bebc88072f930bc'
